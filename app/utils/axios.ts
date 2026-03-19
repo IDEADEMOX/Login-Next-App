25d136a9ea0e94d0d9200cd8fb9f29345d0ad8fb
zhengjialux <15879398507@163.com>
2026-03-19 17:08:39 +0800

    注释外部请求地址，请求本地

--- app/utils/axios.ts
+++ app/utils/axios.ts
@@ -3,7 +3,7 @@ import { generateSignature } from ".";
 
 // ==================== 创建 axios 实例 ====================
 const instance = axios.create({
-  baseURL: "http://localhost:3001",
+  // baseURL: "http://localhost:3001",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
